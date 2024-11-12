c8d92be23f3be1608a62b01d46ae172922682357
HyunJun Yoo <65805176+guswns00123@users.noreply.github.com>
2024-11-12 18:42:39 +0900

    코드 포맷 자동화 (Black)

--- plugins/operators/lotto_api_to_csv_operator.py
+++ plugins/operators/lotto_api_to_csv_operator.py
@@ -1,29 +1,30 @@
 from airflow.models.baseoperator import BaseOperator
 from airflow.hooks.base import BaseHook
-import pandas as pd 
+import pandas as pd
+
 
 class LottoApiToCsvOperator(BaseOperator):
-    template_fields = ('endpoint', 'path', 'file_name', 'base_dt')
+    template_fields = ("endpoint", "path", "file_name", "base_dt")
 
-    #www.dhlottery.co.kr/common.do?method=getLottoNumber&drwNo=회차번호
+    # www.dhlottery.co.kr/common.do?method=getLottoNumber&drwNo=회차번호
     def __init__(self, dataset_nm, path, file_name, base_dt=None, **kwargs):
         super().__init__(**kwargs)
-        self.http_conn_id = 'www.dhlottery.co.kr'
+        self.http_conn_id = "www.dhlottery.co.kr"
         self.path = path
         self.file_name = file_name
-        self.endpoint = 'common.do?method=getLottoNumber&drwNo'
+        self.endpoint = "common.do?method=getLottoNumber&drwNo"
         self.base_dt = base_dt
 
     def execute(self, context):
         import os
 
         connection = BaseHook.get_connection(self.http_conn_id)
-        self.base_url = f'http://{connection.host}/{self.endpoint}'
+        self.base_url = f"http://{connection.host}/{self.endpoint}"
         total_row_df = pd.DataFrame()
 
         start_drwNo = 1
         while True:
-            self.log.info(f'시작:{start_drwNo}')
+            self.log.info(f"시작:{start_drwNo}")
             row_df = self._call_api(self.base_url, start_drwNo)
             print(row_df)
             total_row_df = pd.concat([total_row_df, row_df])
@@ -33,30 +34,45 @@ class LottoApiToCsvOperator(BaseOperator):
                 start_drwNo += 1
         if not os.path.exists(self.path):
             print("check!!")
-            os.system(f'mkdir -p {self.path}')
-        total_row_df.to_csv(self.path + '/' + self.file_name, encoding='utf-8', index=False)
+            os.system(f"mkdir -p {self.path}")
+        total_row_df.to_csv(
+            self.path + "/" + self.file_name, encoding="utf-8", index=False
+        )
 
     def _call_api(self, base_url, drwNo):
         import requests
-        import json 
+        import json
 
-        headers={'Content-Type': 'application/json',
-                'charset': 'utf-8',
-                'Accept': '*/*'}
+        headers = {
+            "Content-Type": "application/json",
+            "charset": "utf-8",
+            "Accept": "*/*",
+        }
         print("111")
-        request_url = f'{base_url}={drwNo}'
-        print(request_url) 
+        request_url = f"{base_url}={drwNo}"
+        print(request_url)
         if self.base_dt is not None:
-            request_url = f'{base_url}={drwNo}'
-        
+            request_url = f"{base_url}={drwNo}"
+
         response = requests.get(request_url, headers)
-        
+
         contents = json.loads(response.text)
-        
-        selected_columns = ["drwNoDate", "drwtNo1", "drwtNo2","drwtNo3","drwtNo4","drwtNo5","drwtNo6","bnusNo",'returnValue']  # 원하는 칼럼들의 이름
-        selected_values = [contents.get(column_name) for column_name in selected_columns]  # 원하는 칼럼들의 값들
+
+        selected_columns = [
+            "drwNoDate",
+            "drwtNo1",
+            "drwtNo2",
+            "drwtNo3",
+            "drwtNo4",
+            "drwtNo5",
+            "drwtNo6",
+            "bnusNo",
+            "returnValue",
+        ]  # 원하는 칼럼들의 이름
+        selected_values = [
+            contents.get(column_name) for column_name in selected_columns
+        ]  # 원하는 칼럼들의 값들
 
         row_df = pd.DataFrame([selected_values], columns=selected_columns)
-        
 
         return row_df
